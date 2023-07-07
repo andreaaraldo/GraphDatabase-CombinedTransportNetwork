e1cbd37f0285ece2b6d34cd156e7ee08e3e68d03
Victoire Gorge <victo@Air-de-Victoire.lan>
2023-07-07 11:37:19 +0200

    MAJ Parameeters

--- Parameters.py
+++ Parameters.py
@@ -22,29 +22,19 @@ idx_h = 0 # intervient également dans le fichier 'Res_DataFrames.py' plus bas
 h = h0[idx_h] # valeur de h actuelle
 
 densite = 26 # 26 pax/h/km^2, nombre de passagers par km² par heure
-longueur = 4000 # en metres, longeur de la zone de service DRT (rectangulaire)
-largeur = 2000 # en metres, largeur de la zone de service DRT (rectangulaire)
+########################
+# ROYAN
+########################
+if Data == "Data":
+    longueur = 4000 # en metres, longeur de la zone de service DRT (rectangulaire)
+    largeur = 2000 # en metres, largeur de la zone de service DRT (rectangulaire)
+elif Data == "small_instance":
+    longueur = 4000 # en metres, longeur de la zone de service DRT (rectangulaire)
+    largeur = 200 # en metres, largeur de la zone de service DRT (rectangulaire)
+
 vitesse_DRT = 30 # en km/h, vitesse moyenne des vehicules DRT
 
 # liste des stop_id des stations DRT
-
-################################################################################################
-# SAVOIR OÙ PLACER LES DRT GRÂCE À L'HEURISTIQUE
-################################################################################################
-
-# Chemin vers votre fichier .txt
-chemin_fichier = os.path.normpath('Heuristic/DRT_heuristique_simple.txt')
-
-# Lire le fichier .txt
-with open(chemin_fichier, 'r') as fichier:
-    fichier = fichier.readlines()
-
-# Extraire la liste à la ligne numéro m
-if nb_DRT  < len(fichier):
-    liste_stations_DRT = eval(fichier[nb_DRT])
-else:
-    print("Nous n'avons pas calculé l'heuristic pour ", nb_DRT , " DRT")
-################################################################################################
 
 ## Intervenant dans le fichier 'PCC_totaltimes_DataFrames.py' :
 ray_max = 10000 # en metres, rayon max auquel doivent appartenir les stations pour etre une destination du centroide
@@ -55,3 +45,27 @@ ray_min = 2000 # en metres, rayon min auquel doivent appartenir les stations pou
 h0_str = ['1_min', '2_min', '4_min', '16_min', '32_min', '64_min'] # format String pour le nom des fichiers
 # idx_h = 0
 h_str = h0_str[idx_h] # valeur de h actuelle
+
+
+################################################################################################
+# SAVOIR OÙ PLACER LES DRT GRÂCE À L'HEURISTIQUE
+################################################################################################
+
+# Chemin vers votre fichier .txt
+chemin_fichier = os.path.normpath('./Results_{}/Placement_DRT/DRT_heuristique_simple.txt'.format(Data))
+
+if nb_DRT == 0:
+    liste_stations_DRT = []
+else :
+    if os.path.exists(chemin_fichier):
+        # Lire le fichier .txt
+        with open(chemin_fichier, 'r') as fichier:
+            fichier = fichier.readlines()
+
+        # Extraire la liste à la ligne numéro m
+        if nb_DRT  < len(fichier):
+            liste_stations_DRT = eval(fichier[nb_DRT])
+        else:
+            print("Nous n'avons pas calculé l'heuristic pour ", nb_DRT , " DRT")
+
+################################################################################################
