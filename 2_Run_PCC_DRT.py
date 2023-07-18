050111228763f93fdd6252b9f794ca72995aa4cb
Victoire Gorge <victo@MacBook-Air-de-Victoire.local>
2023-07-18 20:37:50 +0200

    Epuré

--- 2_Run_PCC_DRT.py
+++ 2_Run_PCC_DRT.py
@@ -1,11 +1,4 @@
-'''
-AVANT D'EXECUTER CE FICHIER :
-- changer *liste_stations_DRT* dans Parameters.py avec le placement des DRT (choisir l'heuristique)
-'''
-
 import subprocess
-import Parameters
-import matplotlib.pyplot as plt
 
 # Liste des fichiers à exécuter dans l'ordre, après avoir changé *liste_stations_DRT* dans Parameters.py
 fichiers = ['Load_DRT.py', 'PCC_optimized.py', 'Res_DataFrames.py', 'Create_Graph_acc_DRT.py']
@@ -15,7 +8,3 @@ for fichier in fichiers:
     print("##########################\n \n Executing ", fichier, "... \n \n##########################")
     # Exécuter le fichier
     subprocess.run(['python3', fichier], check=True)
-
-#Renommer les fichiers qui viennent d'être créés afin de leur donner du sens
-h = Parameters.h
-nb_DRT = Parameters.nb_DRT
